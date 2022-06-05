06f04fea6d805a33419b4194be343e5d7732192f
Hayoon <gkdbssla97@gmail.com>
2022-06-05 19:00:19 +0900

    final pseudo code

--- Final_term.c
+++ Final_term.c
@@ -145,3 +145,152 @@ Alg dequeue() {
     putnode(p)
     return
 }
+
+Alg preOrderSucc(v) {
+    if(isInternal(v))
+        return leftChild(v)
+    p <- parent(v)
+    while(leftChild(p) != v) {
+        if(isRoot(p))
+            invalidNodeException()
+        v <- p
+        p <- parent(p)
+    }
+    return rightChild(p)
+}
+Alg inOrderSucc(v) {
+    if(isInternal(v))
+        v <- rightChild(v)
+        while(isInternal(v))
+            v <- leftChild(v)
+        return v
+    p <- parent(v)
+    while(leftChild(p) != v) {
+        if(isRoot(p))
+            invalidNodeException()
+        v <- p
+        p <- parent(p)
+    } return p
+}
+Alg postOrderSucc(v) {
+    if(isRoot(v))
+        invalidNodeException()
+    p <- parent(v)
+    if(rightChild(p) = v)
+        return p
+    v <- rightChild(p)
+    while (!isExternal(p))
+        v <- leftChild(v)
+    return v
+}
+Alg height(v) {
+    if (isExternal(v))
+        return 0
+    else {
+        h <- max(height(leftChild(v)), rightChild(v))
+                return 1 + h
+    }
+}
+Alg levelOrder(v) {
+    Q <- empty queue
+    Q.enqeueu(v)
+    while(!Q.isEmpty()) {
+        v <- Q.dequeue
+        visit(v)
+        for each w [- children(v)
+            Q.enqueue(w)
+    return
+    }
+}
+Alg evalExpr(v) {
+    if(isExternal(v))
+        return element(v)
+    else {
+        x <- evalExpr(leftChild(v))
+        y <- evalExpr(rightChild(v))
+        @ <- element(v)
+        return x @ y
+    }
+}
+Alg rDBT(v, rank) { // 중위순회
+    if(isInternal(v))
+        rDBT(leftChild(v), rank)
+    rank <- rank + 1
+    drawNodeXY(v, rank, depth(v))
+    if(isInternal(v))
+        rDBT(rightChild(v), rank)
+}
+Alg printExpr(v) {
+    if(isInternal(v))
+        write('(')
+        printExpr(leftChild(v))
+    write(element(v))
+    if(isInternal(v))
+        printExpr(rightChild(v))
+        write(')')
+}
+Alg eulerTour(v) {
+    visitLeft(v)
+    while(isInternal(v))
+        eulerTour(leftChild(v))
+    visitBelow(v)
+    while(isInternal(v))
+        eulerTour(rightChild(v))
+    visitRight(v)
+}
+Alg preOrderSucc(v) {
+    if(isInternal(v))
+        return leftChild(v)
+    p <- parent(v)
+    while(leftChild(p) != v) {
+        if(isRoot(p))
+            invalidNodeException()
+        v <- p
+        p <- parent(p)
+    } return rightChild(p)
+}
+Alg InorderSucc(v) {
+    if(isInternal(v)) {
+        v <- rightChild(v)
+        while(isInternal(v))
+            v <- leftChild(v)
+        return v
+    p <- parent(v)
+    while(leftChild(p) != v)
+        if(isRoot(p))
+            invalidNodeException()
+        v <- p
+        p <- parent(p)
+    } return p
+}
+Alg PostOrderSucc(v) {
+    if(isInternal(v))
+        invalidNodeException()
+    p <- parent(v)
+    if(rightChild(p) = v)
+        return p
+    v <- rightChild(p)
+    while(!isExternal(v))
+        v <- leftChild(v)
+    return v
+}
+
+Alg makeExternalNode() {
+    v <- getnode()
+    v.elem <- read()
+    v.left, v.right <- 0
+    return v
+}
+Alg makeInternalNode() {
+    v <- getnode()
+    v.elem <- read()
+    if(read() = "yes")
+        v.left <- makeInternalNode()
+    else
+        v.left <- makeExternalNode()
+    if(read() = "no")
+        v.right <- makeInternalNode()
+    else
+        v.right <- makeExternalNode()
+    return v
+}
