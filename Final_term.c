d3e9779e3ca309b67b8ea8b068f0110290304193
Hayoon <gkdbssla97@gmail.com>
2022-06-08 00:35:40 +0900

    final pseudo code

--- Final_term.c
+++ Final_term.c
@@ -294,3 +294,134 @@ Alg makeInternalNode() {
         v.right <- makeExternalNode()
     return v
 }
+
+TreeNode *search(TreeNode *node, int key) {
+    if(node == NULL) return NULL;
+    if(key == node->key) return node;
+    else if (key < node->key)
+        return search(node->left, key);
+    else
+        return search(node->right, key);
+}
+TreeNode *search(TreeNode *node, int key) {
+    while(node != NULL) {
+        if(key == node->key) return node;
+        else if(key < node->key)
+            node = node->left;
+        else
+            node = node->right;
+    } return NULL;
+}
+insert_node(T,z) {
+    p <- NULL;
+    t <- root;
+    while t != NULL do
+        p <- t
+        if z->key < p->key
+            then t <- p->left
+            else t <- p->right
+    if p = NULL
+        then root <- z
+        else if z->key < p->key
+            then p->left <- z
+            else p->right <- z
+}
+Alg romanSize(v) {
+    if(isExternal(v))
+        return 1
+    l <- romanSize(leftChild(v))
+    if(l = 0)
+        return 0
+    r <- romanSize(rightChild(v))
+    if((r > 0) & (|l-r| <= 5))
+        return l + r + 1
+    else
+        return 0
+}
+Alg Sibling(v) {
+    p <- v.parent
+    if(p.left = v)
+        return p.right
+    else
+        return p.left
+}
+Alg children(v) {
+    C <- 0
+    c <- v.first
+    while(c != 0)
+        C <- C U {c}
+        c <- c.next
+    return C
+}
+#분리집합
+Alg find(e) {
+    return S[e] // Total O(1)
+}
+Alg union(x, y) {
+    for i <- 0 to n - 1
+        if(S[i] = y)
+            S[i] <- x
+    return  // Total O(n)
+}
+Alg find(e) {
+    return setid((node(e).set).elem)
+}
+Alg union(A, B) {
+    if(S[A].size < S[B].size)
+        smallerSet, largerSet <- A, B
+    else
+        smallerSet, largerSet <- B, A
+    headS, tailS <- S[smallerSet].head, S[smallerSet].tail
+    headL, tailL <- S[largerSet].head, S[largerSet].tail
+    p <- headS
+    while(p != 0) {
+        p.set <- headL
+        p <- p.next
+    }
+    tail.next <- headS
+    S[largerSet].tail <- tailS
+    S[largerSet].size <- S[A].size + S[B].size
+    S[smallerSet].head, S[smallerSet].tail <- 0
+    S[smallerSet].size <- 0
+
+    return //Total O(min(|A|, |B|)
+}
+
+Alg union(A, B) {
+    if(S[A].size < S[B].size)
+        smallerSet, largerSet <- A, B
+    else
+        smallerSet, largerSet <- B, A
+
+    headS, tailS <- S[smallerSet].head, S[smallerSet].tail
+    headL, tailL <- S[largerSet].head, S[largerSet].tail
+
+    p <- headS
+    while(p != 0) {
+        p.set <- headL
+        p <- p.next
+    }
+    tail.next <- headS
+    S[largerSet].tail <- tailS
+    S[largerSet].size <- S[A].size + S[B].size
+    S[smallerSet].head, S[smallerSet].tail <- 0
+    S[smallerSet].size <- 0
+}
+Alg union(x, y) {
+    if(Size[x] < Size[y]) {
+        Parent[x] <- y
+        Size[y] <- Size[x] + Size[y]
+    }
+    else
+        parent[y] <- x
+        Size[x] <- Size[x] + Size[y]
+    reurn
+}
+threadedPointer insucc(threadedPointer tree) {
+    threadedPointer tmp;
+    tmp = tree->rightChild;
+    if (!tree->rightTread) // false
+        while (!temp->leftThread) // false
+            tmp = tmp->leftChild;
+    return tmp;
+}
